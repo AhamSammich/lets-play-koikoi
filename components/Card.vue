e168ad33088083c64e299faeaf8e80b0a5a543c9
AhamSammich <kuro.to.red@gmail.com>
2023-02-16 12:56:12 -1000

    Show matched cards on table

--- components/Card.vue
+++ components/Card.vue
@@ -1,31 +1,38 @@
 <script setup lang="ts">
+import { STORE } from "./composables/game";
+import { getFlower } from "~~/assets/scripts/match";
+
 const props = defineProps<{
   name: string;
   hide?: boolean;
 }>();
 
 const emits = defineEmits(["card-select"]);
+const previewCard = STORE.usePreview();
+
+const isMatched = () => getFlower(props.name) === getFlower(previewCard.value);
 
 // Single-click to preview card matches
 function handleClick(e: Event) {
-  (<HTMLElement>e.target).classList.add("previewed");
+    previewCard.value = props.name;
 }
 
 // Click and hold (2 sec) to select/play card
 async function handlePointerDown(e: Event) {
   console.log("Selecting...");
   let target = <HTMLElement>e.target;
-  target.classList.add("selected");
-  target.addEventListener("pointerup", cancelPointerDown, { once: true });
-  await new Promise((resolve) => setTimeout(resolve, 1500));
-  if (!target.classList.contains("selected")) return;
-  target.classList.remove("previewed");
-  emits("card-select", props.name);
+    target.classList.add("selected");
+    target.addEventListener("pointerup", cancelPointerDown, { once: true });
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    
+    if (!target.classList.contains("selected")) return;
+    emits("card-select", props.name);
+    previewCard.value = "";
 }
 
 async function cancelPointerDown(e: Event) {
   (<HTMLElement>e.target).classList.remove("selected");
-    console.log('...selection cancelled.');;
+  console.log("...selection cancelled.");
 }
 
 onUpdated(() =>
@@ -37,17 +44,14 @@ onUpdated(() =>
 </script>
 
 <template>
-  <template v-if="hide">
-    <div class="card down"></div>
-  </template>
-  <template v-else>
-    <img
-      class="card"
-      :src="`cards/${props.name}.png`"
-      @click="handleClick"
-      @pointerdown="handlePointerDown"
-    />
-  </template>
+  <div v-if="hide" class="card down"></div>
+  <img
+    v-else
+    :class="`card ${isMatched() ? 'previewed' : ''}`"
+    :src="`cards/${props.name}.png`"
+    @click="handleClick"
+    @pointerdown="handlePointerDown"
+  />
 </template>
 
 <style lang="postcss">
@@ -83,6 +87,9 @@ onUpdated(() =>
     transition-delay: 1.5s;
   }
 
+  &.previewed {
+    scale: 1.2;
+  }
 }
 
 @keyframes dropIn {
--- components/composables/game.ts
+++ components/composables/game.ts
@@ -5,6 +5,7 @@ export const STORE: Record<string, () => Ref> = {
     useCurrentOya: () => useState("currentOya", () => 1),
     useScore1: () => useState("score1", () => 0),
     useScore2: () => useState("score2", () => 0),
+
     useHand1: () => useState("hand1", () => <string[]>[]),
     useHand2: () => useState("hand2", () => <string[]>[]),
     useField: () => useState("field", () => <string[]>[]),
@@ -12,13 +13,10 @@ export const STORE: Record<string, () => Ref> = {
     useCollection2: () => useState("collection2", () => <string[]>[]),
     useYaku1: () => useState("yaku1", () => <Dict>{}),
     useYaku2: () => useState("yaku2", () => <Dict>{}),
+
     useActiveP: (): Ref<string> => useState("activeP", () => "p1"),
     useWait: () => useState("waiting", () => false),
     useStart: () => useState("start", () => false),
-}
 
-export function addState(stateName: string, initVal: unknown) {
-    let prop = `use${stateName[0].toUpperCase}${stateName.slice(1)}`;
-    Object.defineProperty(STORE, prop, () => useState(stateName, () => initVal));
-    return STORE[prop];
+    usePreview: (): Ref<string> => useState("cardPreview", () => ""),
 }
