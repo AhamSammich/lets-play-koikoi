bf05062720b47cef54089c5486e01684eaa90c3b
AhamSammich <kuro.to.red@gmail.com>
2023-03-06 18:15:37 -1000

    feat(menu): integrate 'how-to-play' guide

---
+++ components/menu/Guide.vue
@@ -0,0 +1,181 @@
+<script setup lang='ts'>
+defineProps<{
+    isOpen: boolean;
+}>();
+
+let currentIndex = ref(0);
+let sectionArr: Element[];
+
+function handleNext() {
+    if (currentIndex.value < sectionArr.length - 1) {
+        currentIndex.value++;
+    }
+    if (currentIndex.value === sectionArr.length - 1) {
+    }
+}
+
+function handlePrev() {
+    if (currentIndex.value > 0) {
+        currentIndex.value--;
+    }
+    if (currentIndex.value === 0) {
+    }
+}
+
+onMounted(() => sectionArr = [...document.querySelectorAll("#how-to-play section")]);
+</script>
+
+<template>
+    <article id="how-to-play" v-if="isOpen">
+        <section :class="{ active: currentIndex === 0 }">
+            <div>
+                <h1>You've been dealt 8 cards...</h1>
+                <p>Pick a card from your hand to match one of the cards on the table.</p>
+                <p>If none of your cards match, pick one to add to the table.</p>
+            </div>
+            <img src="~/assets/gifs/lpk-how-to-play-a-card.gif" alt="GIF showing how to play a card" />
+        </section>
+        <section :class="{ active: currentIndex === 1 }">
+            <div>
+                <h1>Draw the top card off the deck...</h1>
+                <p>Match the revealed card with a card on the table.</p>
+                <p>Again, if there's no match, just add the card to the table.</p>
+            </div>
+            <img src="~/assets/gifs/lpk-how-to-finish-turn.gif" alt="GIF showing how to play a card" />
+        </section>
+        <section :class="{ active: currentIndex === 2 }">
+            <div>
+                <h1>Collect your cards...</h1>
+                <p>Any matches you've made will be added to your collection.</p>
+                <p>Your collected cards are visible on the bottom-right corner of the screen.</p>
+            </div>
+            <img src="~/assets/gifs/lpk-how-to-finish-turn.gif" alt="GIF showing how to play a card" />
+        </section>
+        <section :class="{ active: currentIndex === 3 }">
+            <div>
+                <h1>Complete a winning hand!</h1>
+                <p>Your goal is to collect cards to complete a set, called a "yaku", and score points before your opponent.</p>
+                <p>Check your progress toward possible yaku using the button below the menu.</p>
+            </div>
+            <img src="~/assets/gifs/lpk-how-to-check-progress.gif" alt="GIF showing how to play a card" />
+        </section>
+        <section :class="{ active: currentIndex === 4 }">
+            <div>
+                <h1>Go big or go home?</h1>
+                <p>Upon completing a yaku, you'll be given the option to continue the round (calling "Koi Koi").</p>
+                <p>This gives you the opportunity to score more points by improving your yaku or completing more.</p>
+            </div>
+            <img src="~/assets/gifs/lpk-how-to-call-koikoi.gif" alt="GIF showing how to play a card" />
+        </section>
+        <section :class="{ active: currentIndex === 5 }">
+            <div>
+                <h1>Be careful though...</h1>
+                <p>Calling "Koi Koi" gives a bonus multiplier to ANY player that scores next.</p>
+                <p>The player with the most points by the end of all rounds wins the game!</p>
+            </div>
+            <img src="~/assets/gifs/lpk-how-to-get-wrecked.gif" alt="GIF showing how to play a card" />
+        </section>
+        <!-- <section :class="{ active: currentIndex === 6 }">
+            <div>
+                <h1>Let's play!</h1>
+                <ol class="list-decimal">
+                    <li>Match cards.</li>
+                    <li>Complete yaku.</li>
+                    <li>Score points.</li>
+                    <li>Have fun!</li>
+                </ol>
+            </div>
+            <img src="~/assets/gifs/lpk-how-to-get-wrecked.gif" alt="GIF showing how to play a card" />
+        </section> -->
+        <footer class="flex justify-end gap-12 w-max absolute right-12 bottom-0 text-yellow-100">
+            <button :disabled="currentIndex === 0" @click="handlePrev()"><Icon name="mdi:arrow-left" /></button>
+            <button :disabled="currentIndex === sectionArr?.length - 1" @click="handleNext()"><Icon name="mdi:arrow-right" /></button>
+        </footer>
+    </article>
+</template>
+
+<style scoped lang="postcss">
+article {
+    position: relative;
+    width: 100%;
+    min-width: 300px;
+    height: 90vh;
+    height: 90dvh;
+    padding: 1rem;
+    background: var(--menu-gray2);
+    z-index: 60;
+    overflow-y: scroll;
+}
+
+h1 {
+    font-family: "Mochiy Pop One";
+}
+
+section {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem;
+    padding-top: 3rem;
+    position: absolute;
+    top: 0;
+    right: 0;
+    opacity: 0;
+
+    & div {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        & p {
+            padding: 0 1rem;
+        }
+    }
+
+    &.active {
+        opacity: 1;
+    }
+}
+
+img {
+    max-height: 60%;
+    border-radius: 0.3rem;
+    position: absolute;
+    bottom: 40px;
+}
+
+.icon {
+    font-size: x-large;
+}
+
+[disabled] {
+    opacity: 0.1;
+    pointer-events: none;
+}
+
+@media (orientation: landscape) {
+    section {
+
+        display: grid;
+        grid-template-columns: 60% 1fr;
+        padding: 2rem;
+        padding-top: 1rem;
+    }
+
+    img {
+        position: static;
+        max-height: 80vh;
+    }
+
+    footer {
+        justify-content: center;
+        align-items: flex-start;
+        left: 25%;
+        gap: 2rem;
+        width: max-content;
+    }
+}
+</style>
