5e0a0f197ebcf854d08d0e97a18581bd1c360350
AhamSammich <kuro.to.red@gmail.com>
2023-03-22 16:48:38 -1000

    refactor: break up main loop into multiple functions

--- components/Hand.vue
+++ components/Hand.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
 import { usePlayerStore } from "~~/stores/playerStore";
+import { useTableStore } from "~~/stores/tableStore";
 
 const props = defineProps<{
   player: string;
@@ -12,6 +14,9 @@ const playerStore = usePlayerStore();
 
 // const cards = props.player === "p1" ? STORE.useHand1() : STORE.useHand2();
 const cards = computed(() => playerStore[props.player].cardsInHand);
+const tableStore = useTableStore();
+const { cardSelected, matchSelected } = storeToRefs(tableStore);
+const selectingMatch = computed(() => cardSelected.value && !matchSelected.value);
 
 // If match, props.cards will be updated
 function selectCard(cardName: string) {
@@ -21,7 +26,7 @@ function selectCard(cardName: string) {
 </script>
 
 <template>
-  <div class="hand">
+  <div :class="{ hand: true, 'pointer-events-none opacity-70': selectingMatch }">
     <template v-if="player === 'p1'">
       <!-- Get selected card details and check table for match -->
       <Card
@@ -46,6 +51,7 @@ function selectCard(cardName: string) {
   gap: 0.3rem;
   margin-left: 0.5rem;
   transform-origin: left;
+  transition: opacity 0.5s;
 }
 
 @media (width < 800px) {
